a3363218e5903f77fea92d06d5c9ca7c1a1abada
lachulok777 <lachulok001@gmail.com>
2021-07-26 01:18:07 -0500

    first push

---
+++ src/test/java/base/to/pom/Base.java
@@ -0,0 +1,30 @@
+package base.to.pom;
+
+public class Base {
+	
+	private void first() {
+		System.out.println("hi");
+		
+	}
+	
+	private void second() {
+
+		System.out.println("I'M");
+	}
+	
+	private void third() {
+		System.out.println("vEDHA");
+
+	}
+
+	
+public static void main(String[] args) {
+	
+	       Base M = new Base();
+	       
+	       M.first();
+	       M.second();
+	       M.third();
+}
+
+}
